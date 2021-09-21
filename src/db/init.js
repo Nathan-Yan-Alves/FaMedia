6d50d6de1e9b7aafd3e06be83de12323f2977ca6
Nathan Alves <82852501+Nathan-Yan-Alves@users.noreply.github.com>
2021-09-21 20:45:28 -0300

    Deleted configs for safety

--- src/db/init.js
+++ src/db/init.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyANtEtpk3BjYbrpDyk1N8i5_SwOZFrCsC0",
-    authDomain: "famedia-1161a.firebaseapp.com",
-    projectId: "famedia-1161a",
-    storageBucket: "famedia-1161a.appspot.com",
-    messagingSenderId: "576711616089",
-    appId: "1:576711616089:web:4b3a35728983ac380bb348",
-    measurementId: "G-Z13Z37YWTS",
+    
 };
 
 const app = initializeApp(firebaseConfig);
